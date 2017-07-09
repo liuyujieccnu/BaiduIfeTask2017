960059128f5695eab01b4f4cf868c5461c583785
liuyujieccnu <liuyujie@mails.ccnu.edu.cn>
2017-07-09 12:17:05 +0800

    2017年7月9日上午实现任务16输入功能

--- task16/js/task.js
+++ task16/js/task.js
@@ -9,21 +9,48 @@
  *    "上海": 40
  * };
  */
-var aqiData = {};
+var aqiData = new Array;
 
 /**
  * 从用户输入中获取数据，向aqiData中增加一条数据
  * 然后渲染aqi-list列表，增加新增的数据
  */
-function addAqiData() {
-
+function addAqiData(){
+    var strCity = document.getElementById("aqi-city-input").value.trim();
+    var strNum = document.getElementById("aqi-value-input").value.replace(/^(\s|\u00A0)+|(\s|\u00A0)+$/g, "");
+    if(!strCity.match(/^[A-Za-z\u4E00-\u9FA5]+$/)){
+        alert("城市名称请输入中文或者英文字符");
+        return;
+    }
+    if (!strNum.match(/^\d+$/)) {
+        alert("空气质量请输入整数");
+        return;
+    }
+    aqiData.push([strCity,Number(strNum)]);
 }
 
 /**
  * 渲染aqi-table表格
  */
 function renderAqiList() {
-
+    var table = document.getElementById("aqi-table");
+    table.innerHTML = "";
+    if(table.childElementCount===0){
+        var thStr = "<tr>"+"<td>"+"城市"+"</td>"+"<td>"+"空气质量"+"</td>"+"<td>"+"操作"+"</td>"+"</tr>";
+        table.innerHTML=thStr;
+    }
+    for(var i=0;i<aqiData.length;i++){
+        var tr=table.appendChild(document.createElement("tr"));
+        var newStrCity = document.createElement("td");
+        newStrCity.innerText = aqiData[i][0];
+        tr.appendChild(newStrCity);
+        var newAqiNum = document.createElement("td");
+        newAqiNum.innerText = aqiData[i][1];
+        tr.appendChild(newAqiNum);
+        var del = document.createElement("td");
+        del.innerHTML = "<button>"+"删除"+"</button>";
+        tr.appendChild(del);
+    }
 }
 
 /**
@@ -48,7 +75,7 @@ function delBtnHandle() {
 function init() {
 
     // 在这下面给add-btn绑定一个点击事件，点击时触发addBtnHandle函数
-
+    document.getElementById("add-btn").addEventListener("click",addBtnHandle);
     // 想办法给aqi-table中的所有删除按钮绑定事件，触发delBtnHandle函数
 
 }
